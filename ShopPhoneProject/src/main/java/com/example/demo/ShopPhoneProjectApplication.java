2b6d2431d4622928eee232e6e15d62890e069b6f
ponleunhoem@gmail.com <ponleunhoem@gmail.com>
2022-10-31 16:51:27 +0700

    projectphone

---
+++ ShopPhoneProject/src/main/java/com/example/demo/ShopPhoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPhoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPhoneProjectApplication.class, args);
+	}
+
+}
